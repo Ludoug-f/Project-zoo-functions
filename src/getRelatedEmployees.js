6c17e186a2b1e19967c051fb08925dbf9d1d3a40
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-03-17 19:23:23 -0300

    req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,15 @@
 const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  const managerCheck = employees.find((element) => id === 'stephanieId');
+  const managerCheck = employees.some((element) => element.managers.includes(id));
   return managerCheck;
 }
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+if (isManager(managerId) === false ){throw new Error('O id inserido não é de uma pessoa colaboradora gerente!')};
+const colab = employees.filter((element) => element.managers.includes(managerId));
+const result = colab.map((element) => `${element.firstName} ${element.lastName}`);
+return result;
 }
-
+console.log(getRelatedEmployees("9e7d4524-363c-416a-8759-8aa7e50c0992"));
 module.exports = { isManager, getRelatedEmployees };
