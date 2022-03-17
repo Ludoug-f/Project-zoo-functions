6a323357d826aa5408a63f565cae5bca925814f7
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-03-17 19:36:08 -0300

    lint e req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,12 +4,13 @@ function isManager(id) {
   const managerCheck = employees.some((element) => element.managers.includes(id));
   return managerCheck;
 }
-// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+
 function getRelatedEmployees(managerId) {
-if (isManager(managerId) === false ){throw new Error('O id inserido não é de uma pessoa colaboradora gerente!')};
-const colab = employees.filter((element) => element.managers.includes(managerId));
-const result = colab.map((element) => `${element.firstName} ${element.lastName}`);
-return result;
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colab = employees.filter((element) => element.managers.includes(managerId));
+  const result = colab.map((element) => `${element.firstName} ${element.lastName}`);
+  return result;
 }
-console.log(getRelatedEmployees("9e7d4524-363c-416a-8759-8aa7e50c0992"));
 module.exports = { isManager, getRelatedEmployees };
