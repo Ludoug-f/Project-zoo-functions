10fd56186c1f9dfcc9d728b57a2babab6a7de76b
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-03-20 18:59:05 -0300

    req 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { data, employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = employees.find((element) => element.id === (id)); // objeto do employee
+  const findAnimal = findEmployee.responsibleFor[0]; // string primeiro animal
+  const objAnimal = species.find((element) => element.id === findAnimal); // objeto primeiro animal
+  const oldestAnimal = objAnimal.residents.sort((a, b) => b.age - a.age);
+  return Object.values(oldestAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
