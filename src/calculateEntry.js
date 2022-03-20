cb93edd35f0002f4ab485fb6371cfe5f95d901c3
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-03-20 18:07:42 -0300

    req 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui...
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      adult += 1;
+    }
+    if (element.age >= 50) {
+      senior += 1;
+    }
+  });
+  const result = { child, adult, senior };
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const func = countEntrants(entrants);
+  return (func.child * prices.child) + (func.adult * prices.adult) + (func.senior * prices.senior);
 }
-
 module.exports = { calculateEntry, countEntrants };
